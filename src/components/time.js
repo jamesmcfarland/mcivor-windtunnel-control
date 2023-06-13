bfa71532e8451bd18ef5e0cc676efac5332e395a
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2023-06-13 14:45:18 +0100

    Fixing default time

--- src/components/time.js
+++ src/components/time.js
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
 const CurrentTime = () => {
-    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentTime(new Date().toLocaleTimeString());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
+  const [currentTime, setCurrentTime] = useState("00:00:00");
+  useEffect(() => {
+    setCurrentTime(new Date().toLocaleTimeString());
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-    return ( <h1>TIME: {currentTime}</h1> );
-}
- 
+  return <h1>TIME: {currentTime}</h1>;
+};
+
 export default CurrentTime;
